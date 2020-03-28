00beb3919029ce74752491f1917a94087fe3c74f
Rob Blumberg <robertblumberg97@gmail.com>
2020-03-28 02:37:30 -0700

    Added function for test

--- tests/test_sbdataextraction.py
+++ tests/test_sbdataextraction.py
@@ -1,8 +1,9 @@
 from sbdataextraction.sbdataextraction import fetch_matches_for_season
 
-season_11_37 = fetch_matches_for_season(11, 37)
 
-# check that season 37 of comp 11 has 7 matches
-assert len(season_11_37) == 7, \
-"""season id 37 of competition id 11 has 7 matches"""
+def test_fetch_matches_for_season():
+    season_11_37 = fetch_matches_for_season(11, 37)
 
+    # check that season 37 of comp 11 has 7 matches
+    assert len(season_11_37) == 7, \
+        """season id 37 of competition id 11 has 7 matches"""
