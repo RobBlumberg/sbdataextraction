cb19db46096cad4ad334a4aac5e69f201e80ca07
Rob Blumberg <robertblumberg97@gmail.com>
2020-03-28 02:30:41 -0700

    Added first test for get_matches_for_season

--- tests/test_sbdataextraction.py
+++ tests/test_sbdataextraction.py
@@ -1,1 +1,8 @@
-from sbdataextraction import sbdataextraction
+from sbdataextraction.sbdataextraction import Game
+from sbdataextraction.sbdataextraction import fetch_matches_for_season
+
+season_11_37 = fetch_matches_for_season(11, 37)
+
+# check that season 37 of comp 11 has 7 matches
+assert len(season_11_37) == 7, \
+"""season id 37 of competition id 11 has 7 matches"""
